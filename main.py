eb4af87929fa41882913745fb65ee1d837dbf71f
Joakim Olsson <joakim.olsson@adicitus.cloud>
2025-10-01 19:54:55 +0000

    Added piping to make the functions available to the LLM

--- main.py
+++ main.py
@@ -4,6 +4,103 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            )
+        }
+    )
+)
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Gets the contents of the specified file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file to get contents from, relative to the working directory."
+            )
+        },
+        required=["file_path"]
+    )
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes the specified content to the specified file path, constrained by the directory. The current content of the file will be overwritten.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file that the content should be written to."
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content that should be written to the file."
+            )
+        },
+        required=["file_path", "content"]
+    )
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Runs the python script file at the provied file_path using the provided arguments (args). Constrained by the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the script file that should be run."
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                description="Positional arguments that should be passed to the script.",
+                items=types.Schema(
+                    description="Argument that should be passed to the script."
+                )
+            )
+        },
+        required=["file_path"]
+    )
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_write_file,
+        schema_run_python_file
+    ]
+)
+
+config=types.GenerateContentConfig(
+    tools=[available_functions],
+    system_instruction=system_prompt
+)
+
 def main(prompt, debug=False, verbose=False):
     load_dotenv()
 
@@ -24,9 +121,14 @@ def main(prompt, debug=False, verbose=False):
         return
     res = client.models.generate_content(
             model="gemini-2.0-flash-001",
-            contents=messages
+            contents=messages,
+            config=config
     )
-    print(res.text)
+    if res.text:
+        print(res.text)
+    if res.function_calls:
+        for call in res.function_calls:
+            print(f"Calling function: {call.name}({call.args})")
 
     if verbose: print(f"Prompt tokens: {res.usage_metadata.prompt_token_count}")
     if verbose: print(f"Response tokens: {res.usage_metadata.candidates_token_count}")
